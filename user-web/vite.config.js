c8d2ed45bbdac6937e10fbc38b268dd80d4ff8f2
刘超群 <liuchaoqun@liuchaoqundeMacBook-Pro.local>
2026-06-10 19:50:50 +0800

    Remove fixed ports to avoid port conflicts

--- user-web/vite.config.js
+++ user-web/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8081,
     proxy: {
       '/api': {
         target: 'http://localhost:8082',
